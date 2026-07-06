8c2fb34fe40c1d3511c7ae3d64738d6db641d933
Mark Wayne Buncaras Menorca <marcuwynu23@gmail.com>
2026-07-07 01:47:18 +0800

    feat: add version command to print the version number

---
+++ cmd/version.go
@@ -0,0 +1,16 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		fmt.Println(Version)
+		return nil
+	},
+}
