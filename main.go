e05101ac01f31bf8f6338a3f6dd433e76dd7f5dc
Mark Wayne Buncaras Menorca <marcuwynu23@gmail.com>
2026-07-07 01:47:40 +0800

    fix: update Execute function to pass version variable

--- main.go
+++ main.go
@@ -2,6 +2,12 @@ package main
 
 import "github.com/marcuwynu23/git-rndocs/cmd"
 
+var (
+	version = "dev"
+	commit  = "none"
+	date    = "unknown"
+)
+
 func main() {
-	cmd.Execute()
+	cmd.Execute(version)
 }
